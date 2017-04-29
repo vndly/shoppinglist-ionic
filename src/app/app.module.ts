5a05c2f64493841902dcddf4e5fbe01a8bef3cdf
Mauricio Togneri <mauricio.togneri@gmail.com>
2017-04-29 23:22:07 +0200

    Code cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,18 @@ import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+	declarations: [
+		MyApp,
+		HomePage
+	],
+	imports: [
+		IonicModule.forRoot(MyApp)
+	],
+	bootstrap: [IonicApp],
+	entryComponents: [
+		MyApp,
+		HomePage
+	],
+	providers: [{ provide: ErrorHandler, useClass: IonicErrorHandler }]
 })
-export class AppModule {}
+export class AppModule { }
